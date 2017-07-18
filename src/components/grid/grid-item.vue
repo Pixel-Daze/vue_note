8f424614a405272f4096b1150843474766b69915
Pixel-Daze <1226093323@qq.com>
2017-07-18 17:16:41 +0800

    add icon

--- src/components/grid/grid-item.vue
+++ src/components/grid/grid-item.vue
@@ -1,10 +1,16 @@
 <template>
 	<div href="javascript:;" class="pixel-grid-item" :style="style">
+		<div class="pixel-gird__icon">
+			<slot name="icon">
+				<span :class="icon"></span>
+			</slot>
+		</div>
 		<slot></slot>
 	</div>
 </template>
 <script>
 	export default {
+		name:'grid-item',
 		computed:{
 			style(){
 				const rows = this.$parent.rows
@@ -13,8 +19,11 @@
 				}else{
 					return
 				}
-				
 			}
+		},
+		props:['icon'],
+		mounted(){
+
 		}
 	}
 </script>
