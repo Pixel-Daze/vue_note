d10e85af37fccc17e22004403f5275ecb8fb3031
Pixel-Daze <1226093323@qq.com>
2017-10-27 20:16:11 +0800

    修改生产环境baseurl

--- src/config/env.js
+++ src/config/env.js
@@ -10,7 +10,7 @@ let routerMode = 'history'
 if(process.env.NODE_ENV == 'development'){
 	baseUrl = '../'
 }else if(process.env.NODE_ENV == 'production'){
-	baseUrl = 'http://production'
+	baseUrl = 'https://pixel-daze.github.io/lodash_test/'
 }
 
 export {
